fa185e29a8646cf33f4f8f187e9a9e77a2f169b6
adharshmk96 <dev.adharshmk96@gmail.com>
2020-05-23 23:40:59 +0530

    handling event misses added for query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,12 +5,36 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
+app.get('/events', (req,res) => {
+    res.send(events);
+})
+
 app.post('/events', async (req,res)=> {
     const event = req.body;
+    events.push(event);
+    console.log(events);
+    
     try{
         await axios.post('http://localhost:4000/events', event);
+    }
+    catch(err) {
+        console.log(err.message)
+    }
+    try{
         await axios.post('http://localhost:4001/events', event);
+    }
+    catch(err) {
+        console.log(err.message)
+    }
+    try{
         await axios.post('http://localhost:4002/events', event);
+    }
+    catch(err) {
+        console.log(err.message)
+    }
+    try{
         await axios.post('http://localhost:4003/events', event);
     }
     catch(err) {
--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const cors = require('cors');
 
 
@@ -16,6 +17,14 @@ app.get('/posts', (req, res) => {
 
 app.post('/events', (req, res) => {
     const { type, data } = req.body;
+
+    handleEvent(type,data);
+
+    res.send({});
+})
+
+
+const handleEvent = (type,data) => {
 
     if (type === 'PostCreated') {
         const { id, title } = data;
@@ -37,12 +46,14 @@ app.post('/events', (req, res) => {
         comment.content = content;
         comment.status = status;
     }
+}
 
-    console.log(posts);
-
-    res.send({});
-})
-
-app.listen(4002, () => {
+app.listen (4002, async () => {
     console.log("Listening on 4002");
-})
+    const res = await axios.get('http://localhost:4005/events');
+
+    for(let event of res.data) {
+        console.log("Processing Event:", event.type)
+        handleEvent(event.type, event.data)
+    }
+});
